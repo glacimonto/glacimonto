b8b059f2773fea0d0cdfd179016c762209bc693e
bessonm <besson.mael@gmail.com>
2019-03-21 23:02:06 +0100

    remove non compiling

--- sorbeto/src/main/java/com/github/glacimonto/sorbeto/domain/running/DefaultRunnerImpl.java
+++ sorbeto/src/main/java/com/github/glacimonto/sorbeto/domain/running/DefaultRunnerImpl.java
@@ -4,11 +4,8 @@ import com.github.glacimonto.sorbeto.domain.ExecutionRequestId;
 
 public class DefaultRunnerImpl implements IRun {
 
-  private Parser parser;
-
   @Override
   public ExecutionRequestId run(String testCase) {
-    Example example = parser.parse(testCase);
     return generateExecutionId();
   }
 
